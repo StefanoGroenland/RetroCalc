7da703de8061c1330316ceb881b3a7973bfc50db
Stefano Groenland <stefanogroenland@Stefanos-MacBook-Pro.local>
2017-04-18 19:17:57 +0200

    Initial Commit

---
+++ RetroCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RetroCalculator
+//
+//  Created by Stefano Groenland on 18/04/2017.
+//  Copyright © 2017 Stefano Groenland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
